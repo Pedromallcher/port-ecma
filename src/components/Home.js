e4a842583ef37741600ae3f3f9a0f74e41bdf96c
Pedromallcher <pedromallcher@gmail.com>
2024-11-05 09:58:04 -0300

    feat(add inf)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,14 @@
 import React, { useRef } from 'react' 
 import CustomHook from './CustomHook';
+import { LiaDeviantart } from "react-icons/lia";
+import { FaBookDead } from "react-icons/fa";
+import { TbGpsFilled } from "react-icons/tb";
+import { IoPersonCircleSharp } from "react-icons/io5";
+import { FaArrowAltCircleDown } from "react-icons/fa";
 function Home() {
 const scrollTab = useRef();
 CustomHook(scrollTab);
+
 
 
 
@@ -14,24 +20,39 @@ return (
             </div>
             <div className="des">
                 {/* 30 */}
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores officiis beatae repellendus rem ullam, ipsam nemo dolorem dolorum illo laborum. Ea sed dolor ab qui, doloremque accusantium esse blanditiis possimus!
+                Sou graduado em Redes de Computadores e atualmente curso Engenharia de Software. Tenho experiência diversificada no setor de tecnologia, atuando em diferentes áreas, desde o desenvolvimento de software até o gerenciamento de infraestruturas de TI. Ao longo da minha carreira, já trabalhei em diversas frentes tecnológicas, adquirindo uma visão ampla e integrada dos desafios e soluções no mundo da tecnologia.
             </div>
             
             <a href="/democv.pdf" target="_blank" rel="noopener noreferrer" className='animation active '>
-                Download My CV
+                Download Currículo <FaArrowAltCircleDown className='iconreact'/>
             </a>
         </div>
         <div className="avatar">
-            <div className="card">
-                <img src="/logo-pedro.png" alt="" />
-                <div className="info">
-                    <div>Desenvolvedor</div>
-                    <div>Brasil </div>
-                    <div>02/12</div>
-                    <div></div>
-                </div>
+    <div className="card">
+        <img src="/logo-pedro.png" alt="Logo Pedro" />
+        <div className="info">
+            <div className="info-item">
+                <LiaDeviantart className='iconreact' />
+                DEV FRONT/BACK
             </div>
+            <div className="info-item">
+            <FaBookDead className='iconreact' />
+                CST REDES
+            </div>
+            <div className="info-item">
+                <TbGpsFilled className='iconreact' />
+                Brasil
+            </div>
+
+            <div className="info-item">
+                < IoPersonCircleSharp className='iconreact' />
+                22 anos
+            </div>
+
+
         </div>
+    </div>
+</div>
     </section>
     )
 }
