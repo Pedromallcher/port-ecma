04e3f92e02a08fb276f0be9e72a6bf7d84016e6f
Pedromallcher <pedromallcher@gmail.com>
2024-11-10 17:29:30 -0300

    feat(...)

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -19,25 +19,27 @@ const scrollTab = useRef();
 CustomHook(scrollTab, divs);
 
   return (
-    <section className='contacts' ref={scrollTab}>
-       <div className="title" ref={(el) => el && divs.current.push(el)}>
-       CONTATO
-       </div>
-       <div className="des" ref={(el) => el && divs.current.push(el)}>
+    <section className="contacts" ref={scrollTab}>
+      <div className="title" ref={(el) => el && divs.current.push(el)}>
+        CONTATO
+      </div>
+
+  
+      <div className="des" ref={(el) => el && divs.current.push(el)}>
         {/* 20 */}
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis quae veniam amet nesciunt voluptatibus quis consectetur consequatur quisquam harum.
-       </div>
-       <div className="list"  ref={(el) => el && divs.current.push(el)}>
-        {
-          listContacts.map((value, key) => (
-            <div className='item' key={key}>
-              <h3>{value.title}</h3>
-              <div>{value.value}</div>
-            </div>
-          ))
-        }
-       </div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+        perspiciatis quae veniam amet nesciunt voluptatibus quis consectetur
+        consequatur quisquam harum.
+      </div>
+      <div className="list" ref={(el) => el && divs.current.push(el)}>
+        {listContacts.map((value, key) => (
+          <div className="item" key={key}>
+            <h3>{value.title}</h3>
+            <div>{value.value}</div>
+          </div>
+        ))}
+      </div>
     </section>
-  )
+  );
 }
 export default Contacts
